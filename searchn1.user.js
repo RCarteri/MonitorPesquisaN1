20cdc163f369339cfe7db9954343f18b3ff4ed89
Eduardo Czamanski Rota <kaname.pf@gmail.com>
2018-03-21 10:02:23 -0300

    Create searchn1.user.js

---
+++ searchn1.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Monitor Pesquisa
+// @version      1.2
+// @description  Auto selecionar os principais projetos Vivo1
+// @author       Eduardo Czamanski Rota, Thauã Corrêa Martins
+// @match        http://10.41.253.34:8080/*
+// @match        http://svuxdsoa53:8080/*
+// @require      https://raw.githubusercontent.com/quesmues/MonitorPesquisaN1/master/searchn1.js
+// @run-at document-idle
+// ==/UserScript==

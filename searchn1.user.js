ed58d320acf44c3b507aa71a34cacc6d253eb9b7
Eduardo Czamanski Rota <kaname.pf@gmail.com>
2018-03-21 10:09:18 -0300

    Update searchn1.user.js

--- searchn1.user.js
+++ searchn1.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Monitor Pesquisa
-// @version      1.2
+// @version      1.3
 // @description  Auto selecionar os principais projetos Vivo1
 // @author       Eduardo Czamanski Rota, Thauã Corrêa Martins
 // @match        http://10.41.253.34:8080/*
 // @match        http://svuxdsoa53:8080/*
 // @require      https://raw.githubusercontent.com/quesmues/MonitorPesquisaN1/master/searchn1.js
 // @run-at document-idle
+// @icon         https://forum.vivo.com.br/attachment.php?attachmentid=737&d=1418039499
 // ==/UserScript==

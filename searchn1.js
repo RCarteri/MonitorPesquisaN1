b1f6833fb316617077ccf62ff974397ab85b695a
Eduardo Czamanski Rota <kaname.pf@gmail.com>
2018-03-21 10:00:38 -0300

    Create searchn1.js

---
+++ searchn1.js
@@ -0,0 +1,20 @@
+
+var projetosx = ['QA_2017','REGRESSAO_2017','2018_PROJ_MOVEL','2018_PROJ_DIGITAL','2018_PP_B2B_FIXA','2018_PROJ_CONVERGENTE_OSS', 'BEATRIX_FASE_2','FS_Beatrix','2018_PP_MOVEL','2018_PROJ_B2B_FIXA','2018_PROJ_TRANSFORMACAO_B2C','TST_CANAIS_2018', 'QA', 'VIVO_AUTOMACAO','2018_PP_CONVERGENTE_OSS'];
+
+setTimeout(function() {
+
+    var check = document.getElementById("checks").parentNode;
+    check = check.childNodes[3];
+    for (i = 0; i < check.childNodes.length; i++) {
+        selectCheck = check.childNodes[i];
+        console.log(selectCheck);
+        console.log(i);
+        selectCheck.childNodes[0].checked = false;
+
+        for (cont = 0; cont < projetosx.length; cont++){
+            if(selectCheck.childNodes[1].innerText === projetosx[cont])
+            selectCheck.childNodes[0].checked = true;
+        }
+    }
+    console.log(check.childNodes.length);
+}, 2000);

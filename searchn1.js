e1b79f26626b5ce5a11a8f8c6f20c4aad2f0d54e
Eduardo Czamanski Rota <kaname.pf@gmail.com>
2018-03-21 10:02:41 -0300

    Update searchn1.js

--- searchn1.js
+++ searchn1.js
@@ -1,4 +1,4 @@
-
+//Alterar os projetos que queira que seja selecionado automáticamente
 var projetosx = ['QA_2017','REGRESSAO_2017','2018_PROJ_MOVEL','2018_PROJ_DIGITAL','2018_PP_B2B_FIXA','2018_PROJ_CONVERGENTE_OSS', 'BEATRIX_FASE_2','FS_Beatrix','2018_PP_MOVEL','2018_PROJ_B2B_FIXA','2018_PROJ_TRANSFORMACAO_B2C','TST_CANAIS_2018', 'QA', 'VIVO_AUTOMACAO','2018_PP_CONVERGENTE_OSS'];
 
 setTimeout(function() {

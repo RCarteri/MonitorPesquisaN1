8e903039b7e0f800027a5e9984c4a99ee6420cfd
RCarteri <38788751+RCarteri@users.noreply.github.com>
2018-04-27 11:10:05 -0300

    Update searchn1.js
    Há outros check list a serem marcados, adicionei eles no final da linha:
    '2018_PROJ_B2C_FIXA', '2018_PP_MOVEL_B2C', '2018_PROJ_TRANSFORMACAO_B2B', '2018_PP_CONVERG_OSS_ATENDEN',
                     '2018_PROJ_MOVEL_B2C', '2018_PROJ_CONVERG_OSS_ATENDEN'
    
    removi da lista:
    '2018_PROJ_MOVEL', '2018_PROJ_CONVERGENTE_OSS', '2018_PP_MOVEL',  '2018_PROJ_B2B_FIXA', '2018_PP_CONVERGENTE_OSS',

--- searchn1.js
+++ searchn1.js
@@ -1,7 +1,8 @@
 //Alterar os projetos que queira que seja selecionado automáticamente
-var projetosx = ['QA_2017','REGRESSAO_2017','2018_PROJ_MOVEL','2018_PROJ_DIGITAL','2018_PP_B2B_FIXA',
-                 '2018_PROJ_CONVERGENTE_OSS', 'BEATRIX_FASE_2','FS_Beatrix','2018_PP_MOVEL','2018_PROJ_B2B_FIXA',
-                 '2018_PROJ_TRANSFORMACAO_B2C','TST_CANAIS_2018', 'QA', 'VIVO_AUTOMACAO','2018_PP_CONVERGENTE_OSS'];
+var projetosx = ['QA_2017','REGRESSAO_2017', '2018_PROJ_DIGITAL', '2018_PP_B2B_FIXA', 
+                 'BEATRIX_FASE_2','FS_Beatrix', '2018_PROJ_TRANSFORMACAO_B2C', 'TST_CANAIS_2018', 'QA', 'VIVO_AUTOMACAO', 
+                 '2018_PROJ_B2C_FIXA', '2018_PP_MOVEL_B2C', '2018_PROJ_TRANSFORMACAO_B2B', '2018_PP_CONVERG_OSS_ATENDEN',
+                 '2018_PROJ_MOVEL_B2C', '2018_PROJ_CONVERG_OSS_ATENDEN'];
 setTimeout(function() {
     var check = document.getElementById("checks"); //Seleciona a div que possui as checkbox
     for (i = 0; i < check.childNodes.length; i++) {
